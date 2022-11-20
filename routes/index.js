59c8bc7d85bf23d1022faab865c431281ae28da7
Damian <a01656277@tec.mx>
2022-11-20 14:55:12 -0600

    Added productos.ejs

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,10 @@ router.get('/reglamento', function(req, res, next) {
   res.render('reglamento', { title: 'Reglamento' });
 });
 
+/* GET home page (reglamento.ejs). */
+router.get('/rentar', function(req, res, next) {
+  res.render('rentar', { title: 'Rentar' });
+});
+
+
 module.exports = router;
