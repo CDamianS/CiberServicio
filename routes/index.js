87e805e38f40e93bd88e18eb80867ac5102d1314
Damian <a01656277@tec.mx>
2022-11-20 14:46:52 -0600

    Added reglamento.ejs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page (index.ejs). */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET home page (reglamento.ejs). */
+router.get('/reglamento', function(req, res, next) {
+  res.render('reglamento', { title: 'Reglamento' });
 });
 
 module.exports = router;
