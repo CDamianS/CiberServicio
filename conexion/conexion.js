28c6ddcb77d645c8686a3033229418fc927a491b
CDamianS <A01656277@tec.mx>
2022-11-30 11:56:48 -0600

    Fixed MySQL integration test

--- conexion/conexion.js
+++ conexion/conexion.js
@@ -1,23 +1,17 @@
-const mysql = require('mysql');
+const mysql = require('mysql')
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'productos'
-});
+  user: 'dbuser',
+  password: 's3kreee7',
+  database: 'my_db'
+})
 
-connection.connect( 
-    (err)=>{
-        if(!err){ console.log("Conexion establecida");}
-        else{ console.log("Conexion fallida");}
-    }
+connection.connect()
 
-);
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+  if (err) throw err
 
-connection.query("SELECT * FROM tblproductos",function(err,resultados){
-    
-    console.log(resultados);
-    
-});
+  console.log('The solution is: ', rows[0].solution)
+})
 
-connection.end();
+connection.end()
