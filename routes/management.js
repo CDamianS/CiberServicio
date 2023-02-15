afe2b26b9bf3ded1cc285727a2fe56f911c79a84
Djmr5 <diegojmr5@gmail.com>
2023-02-15 14:22:09 -0600

    Layout for modifying users data | Not functional

--- routes/management.js
+++ routes/management.js
@@ -21,43 +21,41 @@ router.post('/addUser', function(req, res){
 
   let sql = `INSERT INTO users (matricula, name, role) VALUES ('${req.body.newUserMatr}', '${req.body.newUserName}', '${req.body.newUserRole}')`;
   connection.query(sql, err=>{
-    if(!err)
-    {
+    if(!err){
     console.log('Successfully added user')
     res.redirect('/management')
-    
     }
     else
     console.log(err);
   });
 });
 
-/*
 router.post('/modifyUser', function(req, res){
 
-  let sql = `INSERT INTO users (matricula, name, role) VALUES ('${req.body.newUserMatr}', '${req.body.newUserName}', '${req.body.newUserRole}')`;
+  let idToModf = req.body['doModf'];
+  let matr = req.body['modfMatr'];
+  let name = req.body['modfName'];
+  let role = req.body['modfRole'];
+  let sql = `UPDATE users SET matricula='${matr}', name='${name}', role='${role}' WHERE id=${idToModf}`;
   connection.query(sql, err=>{
-    if(!err)
-    {
-    console.log('Successfully added user')
-    res.redirect('/management')
-    
+    if(!err){
+      console.log('Successfully added user')
+      res.redirect('/management')
     }
     else
     console.log(err);
   });
 });
-*/
+
 router.post('/deleteUser', function(req, res){
 
-  let idToDelete= req.body['deleteUserCheckbox']
+  let idToDelete= req.body['deleteUserCheckbox'];
   let sql = `DELETE FROM users WHERE id=${idToDelete}`;
+
   connection.query(sql, err=>{
-    if(!err)
-    {
-    console.log('Successfully deleted user')
-    res.redirect('/management')
-    
+    if(!err){
+      console.log('Successfully deleted user')
+      res.redirect('/management')
     }
     else
     console.log(err);
