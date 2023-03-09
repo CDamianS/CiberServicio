1e9a26f4dbc9e3b162c49d3bca274c28b6b56861
Diego Jacobo Martínez <diegojmr5@gmail.com>
2023-03-08 20:42:38 -0600

    SQL injection minor changes

--- routes/management.js
+++ routes/management.js
@@ -5,17 +5,19 @@ var session = require('express-session');
 var passport = require('passport');
 var ensureLogIn = require('connect-ensure-login').ensureLoggedIn;
 var ensureLoggedIn = ensureLogIn();
-var encrypt = require('bcrypt');
+var bcrypt = require('bcrypt');
 
 /* GET page (management.ejs). */
 router.get('/', ensureLoggedIn, function(req, res, next) {
 
   /* Retrieve of DB data */
-  connection.query("SELECT * FROM users", function(errDB, data){
-
-    /* Rendered page with queried data*/
-    res.render('management', { title: 'Management', usersDB: data });
-        
+  connection.query("SELECT * FROM users", function(err, data){
+    if (!err) {
+      /* Rendered page with queried data*/
+      res.render('management', { title: 'Management', usersDB: data });
+    }
+    else
+    console.log(err);
   });
 });
 
@@ -25,10 +27,11 @@ router.post('/addUser', ensureLoggedIn, async function(req, res){
   let matr = req.body.newUserMatr;
   let name = req.body.newUserName;
   let role = req.body.newUserRole;
-  let hashedPassword = await bcrypt.hash(req.body.pass, 10);
+  let salt = bcrypt.genSaltSync(10);
+  let hashedPassword = await bcrypt.hashSync(req.body.newUserPass, salt);
 
-  let sql = 'INSERT INTO users (matricula, name, password, role) VALUES (?, ?, ?, ?)';
-  connection.query(sql, [matr, name, hashedPassword, role], err=>{
+  let sql = 'INSERT INTO users (matricula, name, password, hash, role) VALUES (?, ?, ?, ?, ?)';
+  connection.query(sql, [matr, name, hashedPassword, hash, role], err=>{
     if(!err){
     console.log('Successfully added user')
     res.redirect('/management')
@@ -39,17 +42,30 @@ router.post('/addUser', ensureLoggedIn, async function(req, res){
 });
 
 /* POST page (management.ejs management/modifyUser). */
-router.post('/modifyUser', ensureLoggedIn, function(req, res){
+router.post('/modifyUser', ensureLoggedIn,function(req, res){
 
   let idToModf = req.body['modfId'];
   let matr = req.body['modfMatr'];
   let name = req.body['modfName'];
   let role = req.body['modfRole'];
-  let sql =' UPDATE users SET matricula=?, name=?, role=? WHERE id=?';
-  connection.query(sql, [matr, name, role, idToModf], err=>{
+  let pass = req.body['modfPass'];
+
+  let sql = 'SELECT hash FROM users WHERE id = ?';
+  connection.query(sql, [idToModf],async function(err, data){
     if(!err){
-      console.log('Successfully modified user')
-      res.redirect('/management')
+
+      let hashedPassword = await bcrypt.hashSync(pass, data[0].hash);
+      sql =' UPDATE users SET matricula=?, name=?, password=?, role=? WHERE id=?';
+
+      connection.query(sql, [matr, name, hashedPassword, role, idToModf], err=>{
+        if(!err){
+          console.log('Successfully modified user')
+          res.redirect('/management')
+        }
+        else
+        console.log(err);
+      });
+
     }
     else
     console.log(err);
@@ -59,7 +75,7 @@ router.post('/modifyUser', ensureLoggedIn, function(req, res){
 /* POST page (management.ejs management/deleteUser). */
 router.post('/deleteUser', ensureLoggedIn, function(req, res){
 
-  let idToDelete= req.body['deleteUserButton'];
+  let idToDelete = req.body['deleteUserButton'];
   let sql = 'DELETE FROM users WHERE id = ?';
 
   connection.query(sql, idToDelete, err=>{
