838dd70c5bdde2c4fe0dbcf986ad64cabb60759f
Diego Jacobo Martínez <diegojmr5@gmail.com>
2023-03-16 09:34:19 -0600

    Updated DB structure and data

--- database/structure.sql
+++ database/structure.sql
@@ -1,31 +1,40 @@
 CREATE DATABASE cyber;
 USE cyber;
 
-CREATE TABLE users (
+CREATE TABLE usuarios (
     id INTEGER AUTO_INCREMENT NOT NULL,
     matricula VARCHAR(20) NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    password(100)
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     role VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
 );
 
+CREATE TABLE clientes (
+    idMatricula INTEGER AUTO_INCREMENT NOT NULL,
+    name VARCHAR(100),
+    timesPlayed INTEGER DEFAULT 0,
+    numSanciones INTEGER DEFAULT 0,
+    PRIMARY KEY(idMatricula)
+);
+
 CREATE TABLE juegos (
     id INTEGER AUTO_INCREMENT,
-    name VARCHAR(127) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    available VARCHAR(5) DEFAULT 'false',
     PRIMARY KEY (id)
 );
 
 CREATE TABLE sanciones (
     idSancion INTEGER AUTO_INCREMENT NOT NULL,
-    cause VARCHAR(255),
-    idMatricula VARCHAR(20) NOT NULL FOREIGN KEY (users/matricula),
+    cause VARCHAR(255) NOT NULL,
+    idMatricula VARCHAR(20) NOT NULL FOREIGN KEY (clients/idMatricula),
     PRIMARY KEY (idSancion)
 );
 
-CREATE TABLE log (
+CREATE TABLE registrosActividad (
     id INTEGER AUTO_INCREMENT NOT NULL,
-    actionPerformed VARCHAR(255),
-    userPerformed VARCHAR(255),
+    actionPerformed VARCHAR(255) NOT NULL,
+    userPerformed VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
