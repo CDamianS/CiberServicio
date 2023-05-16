db7e9c680c4eb3b83f23ab9ef68b52ef94ef3302
Diego Jacobo Martínez <diegojmr5@gmail.com>
2023-05-16 14:44:50 -0600

    Initial Django-SQLite configuration

---
+++ django_sqlite/static/js/main.js
@@ -0,0 +1,29 @@
+// Header Begins
+function resizeNav() {
+    // Set the nav height to fill the window
+    $("#nav-fullscreen").css({"height": window.innerHeight});
+
+    // Set the circle radius to the length of the window diagonal,
+    // this way we're only making the circle as big as it needs to be.
+    var radius = Math.sqrt(Math.pow(window.innerHeight, 2) + Math.pow(window.innerWidth, 2));
+    var diameter = radius * 2;
+    $("#nav-overlay").width(diameter);
+    $("#nav-overlay").height(diameter);
+    $("#nav-overlay").css({"margin-top": -radius, "margin-left": -radius});
+}
+
+// Set up click and window resize callbacks, then init the nav.
+$(document).ready(function() {
+    $("#nav-toggle").click(function() {
+        $("#nav-toggle, #nav-overlay, #nav-fullscreen").toggleClass("open");
+    });
+
+    $(window).resize(resizeNav);
+
+    resizeNav();
+	
+	window.setTimeout(function() {
+		 $("#nav-toggle").click();
+	}, 1000)
+});
+// Header Ends
